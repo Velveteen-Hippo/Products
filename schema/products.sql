499a75f28b91adeaed47ef9b109d49ae806d5cc1
davidkharbin <david_k_harbin@hotmail.com>
2021-06-24 16:01:49 -0600

    tables complete, data loaded from CSVs

--- schema/products.sql
+++ schema/products.sql
@@ -1,48 +1,62 @@
-
-
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS features;
+DROP TABLE IF EXISTS styles CASCADE;
+DROP TABLE IF EXISTS related_products;
+DROP TABLE IF EXISTS photos;
+DROP TABLE IF EXISTS skus;
 
 
 CREATE TABLE products (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(64) NOT NULL,
-  slogan VARCHAR(64) NOT NULL,
-	description VARCHAR(255),
-	category VARCHAR(32),
-	default_price MONEY,
+	name VARCHAR NOT NULL,
+  slogan VARCHAR NOT NULL,
+	description VARCHAR,
+	category VARCHAR,
+	default_price MONEY
 );
 
 CREATE TABLE features (
 	id SERIAL PRIMARY KEY,
 	product_id INT NOT NULL,
-	feature VARCHAR(32),
-	value VARCHAR(32),
+	feature VARCHAR,
+	value VARCHAR,
+	FOREIGN KEY(product_id)
+	REFERENCES products(id)
 );
 
 CREATE TABLE styles (
 	id SERIAL PRIMARY KEY,
 	product_id INT,
-	name VARCHAR(32),
-	sale_price MONEY,
+	name VARCHAR,
+	sale_price VARCHAR,
 	original_price MONEY,
-	default_style BOOLEAN
+	default_style BOOLEAN,
+	FOREIGN KEY(product_id)
+	REFERENCES products(id)
 );
 
-CREATE TABLE related (
+CREATE TABLE related_products (
 	id SERIAL PRIMARY KEY,
 	current_product_id INT,
 	related_product_id INT,
+	FOREIGN KEY(current_product_id)
+	REFERENCES products(id)
 );
 
 CREATE TABLE photos (
 	id SERIAL PRIMARY KEY,
 	style_id INT,
-	url VARCHAR(255),
-	thumbnail_url VARCHAR(255)
+	url VARCHAR,
+	thumbnail_url VARCHAR,
+	FOREIGN KEY(style_id)
+	REFERENCES styles(id)
 );
 
-CREATE TABLE skus(
+CREATE TABLE skus (
 	id SERIAL PRIMARY KEY,
 	style_id INT,
-	size VARCHAR(8)
+	size VARCHAR,
 	quantity SMALLINT,
-)
+	FOREIGN KEY(style_id)
+	REFERENCES styles(id)
+);
