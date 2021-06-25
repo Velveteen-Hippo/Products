86a681357b64ab905dae3e7058fc708943ffdb43
davidkharbin <david_k_harbin@hotmail.com>
2021-06-24 20:30:34 -0600

    node updated to v14.7, basic routes created

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+// const db = require('../database-pg');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/products', function (req, res) {
+  res.send('Retrieves the list of products.')
+})
+
+app.get('/products/:product_id', function (req, res) {
+  res.send('Returns all product level information for a specified product id.')
+})
+
+app.get('/products/:product_id/styles', function (req, res) {
+  res.send('Returns the all styles available for the given product.')
+})
+
+app.get('/products/:product_id/related', function (req, res) {
+  res.send('Returns the id\'s of products related to the product specified')
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+
